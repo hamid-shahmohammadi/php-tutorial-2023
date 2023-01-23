57a53df654f669a727bf7bfb078d73336eadb8bf
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-01-23 10:37:54 +0330

    6.Associative-Arrays

--- index.php
+++ index.php
@@ -49,6 +49,20 @@
         <?php endforeach; ?>
     </ul>
 
+    <?php
+        $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+        // or
+
+        /* comment
+         multi
+         line */
+
+        $age['Peter'] = "35";
+        $age['Ben'] = "37";
+        $age['Joe'] = "43";
+    ?>
+
 </body>
 
 </html>
