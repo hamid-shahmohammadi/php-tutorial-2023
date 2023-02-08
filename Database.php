d4ac9c88cec10d7add305b40d68164aabc38a210
Hamid Shah Mohammadi <43700170+hamid-shahmohammadi@users.noreply.github.com>
2023-02-08 10:55:29 +0330

    18.config

---
+++ Database.php
@@ -0,0 +1,26 @@
+<?php
+ class Database
+ {
+     
+     public $dbh;
+     public $statement;
+ 
+     public function __construct($config,$username='root',$password='')
+     {        
+         $dbs = 'mysql:'.http_build_query($config,'',';');
+        
+         $this->dbh = new PDO($dbs,  $username, $password,[
+             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+         ]);
+       
+     }
+     public function query($query)
+     {
+         $this->statement = $this->dbh->prepare($query);
+ 
+         $this->statement->execute();
+ 
+         return $this->statement;
+     }
+    
+ }
--- index.php
+++ index.php
@@ -1,39 +1,15 @@
 <?php
 
 require "helper.php";
+require "Database.php";
 
 // require "routes.php";
+$config=require('config.php');
 
-class Database
-{
-    public $host = "localhost";
-    public $db = "website";
-    public $user = "root";
-    public $password = "";
-    public $dbh;
-    public $statement;
-
-    public function __construct()
-    {
-        $dbs = "mysql:host=$this->host;dbname=$this->db;charset=utf8mb4";
-        $this->dbh = new PDO($dbs,  $this->user, $this->password);
-        $this->dbh->exec("set names utf8mb4");
-    }
-    public function query($query)
-    {
-        $this->statement = $this->dbh->prepare($query);
-
-        $this->statement->execute();
-
-        return $this->statement;
-    }
-   
-}
-
-$db=new Database();
+$db=new Database($config['database']);
 
 $posts=$db->query("select * from posts")->fetchAll(PDO::FETCH_ASSOC);
-// dd($posts);
+dd($posts);
 
 foreach($posts as $post){
     echo '<li>'.$post['title'].'</li>';
